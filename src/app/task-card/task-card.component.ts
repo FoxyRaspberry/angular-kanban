b672468407e6635e56828ed7dfa6d56cb6d0e097
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2024-12-20 21:03:16 +0300

    feat: handle `taskStatus`

--- src/app/task-card/task-card.component.ts
+++ src/app/task-card/task-card.component.ts
@@ -9,12 +9,25 @@ import { KanbanTask, KanbanTaskStatus } from '../task-list/tasks.service';
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
-  @Input({
-    required: true,
-  }) task: KanbanTask = {
+  @Input({ required: true })
+  set task(task: KanbanTask) {
+    this.taskInternal = task;
+    this.statusCSSModifier = statusCSSModifiersMap.get(task.taskStatus) as string;
+  }
+
+  protected taskInternal: KanbanTask = {
     taskDescription: '',
     taskID: '',
     taskName: '',
     taskStatus: KanbanTaskStatus.ToDo,
   };
+
+  protected statusCSSModifier = statusCSSModifiersMap.get(KanbanTaskStatus.ToDo) as string;
 }
+
+const statusCSSModifiersMap = new Map<KanbanTaskStatus, string>([
+  [KanbanTaskStatus.Done, 'app-task-status_done'],
+  [KanbanTaskStatus.InProgress, 'app-task-status_in-progress'],
+  [KanbanTaskStatus.InReview, 'app-task-status_in-review'],
+  [KanbanTaskStatus.ToDo, 'app-task-status_to-do'],
+]);

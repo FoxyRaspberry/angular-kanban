ba2034c53b13a3ca36329bf4c461b675ca92929a
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2024-12-20 21:04:06 +0300

    feat: use `AsyncPipe`

--- src/app/photo-card/photo-card.component.ts
+++ src/app/photo-card/photo-card.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoCardService } from './photo-card.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-photo-card',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './photo-card.component.html',
   styleUrl: './photo-card.component.scss'
 })
 export class PhotoCardComponent implements OnInit {
-  protected imgSrc = '';
+  protected imgSource = new Observable<string>();
 
   constructor(private readonly photoCardService: PhotoCardService) { }
 
   ngOnInit(): void {
-    this.photoCardService.read()
-      .subscribe((url: string): void => {
-        this.imgSrc = url;
-      })
+    this.imgSource = this.photoCardService.read();
   }
 }
